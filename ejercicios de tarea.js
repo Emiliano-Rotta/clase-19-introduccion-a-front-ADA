bf0c36bd7876c9407ac48b9c0b16e82ebec9200b
Emiliano Rotta <emilianorottaf@gmail.com>
2023-08-10 10:51:37 -0300

    Ejercicios para realizar de tarea

---
+++ ejercicios de tarea.js
@@ -0,0 +1,41 @@
+// Ejercicio 1: Realizar un array que contenga numeros pares del 0 al 20
+// Ejercicio 2: realizar un objeto que contga tu nombre, apellido, tu color favorito, comida favorita
+
+// Ejercicio 3: dado el siguiente array de objetos realizar los ejercicios 
+
+var perros = [
+    {
+        nombre: "Firulais",
+        Raza: "Salchicha",
+        edad: 7,
+        vacunas: {antiRabica:true, moquillo: false},
+        nombreDeLosDueños: ["Jorge", "Marta"]
+    },
+    {
+        nombre: "Negro",
+        Raza: "Labrador",
+        edad: 3,
+        vacunas: {antiRabica:false, moquillo: false},
+        nombreDeLosDueños: ["Emilio", "Josefina"]
+    },
+    {
+        nombre: "Alipio",
+        Raza: "No tiene",
+        edad: 1,
+        vacunas: {antiRabica:true, moquillo: true},
+        nombreDeLosDueños: ["Carlos", "Clara"]
+    },
+    {
+        nombre: "Manchas",
+        Raza: "Dalmata",
+        edad: 2,
+        vacunas: {antiRabica:false, moquillo: true},
+        nombreDeLosDueños: ["Eduardo", "Juan Cruz"]
+    }
+]
+
+// A) Un console.log que muestre el nombre de todos los perros
+// B) un console.log que muestre todas las edades sumadas
+// C) un console.log que muestre el nombre de todos los dueños
+// D) un console.log que muestre si tiene la vacuna antiRabica
+
